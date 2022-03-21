7bc61e925ef0caf937fd2104af5b23b81729155c
carol-alonso <lacerdacarol01@gmail.com>
2022-03-21 16:49:39 -0300

    js atualização

--- Fundamentos/assets/js/script.js
+++ Fundamentos/assets/js/script.js
@@ -1,6 +1,8 @@
 let nome = document.querySelector("#nome");
 let email = document.querySelector("#email");
 let assunto = document.querySelector("#assunto");
+let nomeOk = false;
+let emailOk = false;
 
 function validaNome() {
   let txt = document.querySelector("#txtNome");
@@ -8,14 +10,24 @@ function validaNome() {
     txt.innerHTML = "Nome inválido";
   } else {
     txt.innerHTML = "Nome válido";
+    nomeOk = true
   }
 }
 
-function validaNome() {
+function validaEmail() {
   let txt = document.querySelector("#txtEmail");
-  if (email.value.indexOf('@') == -1) {
+  if (email.value.indexOf("@") == -1) {
     txt.innerHTML = "Email inválido";
   } else {
     txt.innerHTML = "Email válido";
+    emailOk = true
+  }
+}
+
+function enviar() {
+  if (nomeOk == true && emailOk == true) {
+    alert("Formulário enviado!");
+  } else {
+    alert("Preencha o formulário corretamente");
   }
 }
